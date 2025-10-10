b70fad7b2b772c40866774da70944eae3157e0c7
Adrian Juhl <adrian.juhl@gmail.com>
2025-10-10 12:50:31 +1030

    Expose further role parameters in the install script.

--- .extras/bin/install_maven.sh
+++ .extras/bin/install_maven.sh
@@ -93,10 +93,7 @@ install_maven()
 #    --extra-vars="adrianjuhl__maven__archive_file_checksum=sha512:706f01b20dec0305a822ab614d51f32b07ee11d0218175e55450242e49d2156386483b506b3a4e8a03ac8611bae96395fd5eec15f50d3013d5deed6d1ee18225" \
 
   ANSIBLE_PLAYBOOK_COMMAND_OPTIONS=(
-    "${ANSIBLE_CHECK_MODE_ARGUMENT}"
-    "${ANSIBLE_DIFF_MODE_ARGUMENT}"
-    "${ANSIBLE_VERBOSE_ARGUMENT}"
-    "${ASK_BECOME_PASS_OPTION}"
+    "${ANSIBLE_CHECK_MODE_ARGUMENT}" "${ANSIBLE_DIFF_MODE_ARGUMENT}" "${ANSIBLE_VERBOSE_ARGUMENT}" "${ASK_BECOME_PASS_OPTION}"
     "--inventory=localhost,"
     "--connection=local"
     "--extra-vars=adrianjuhl__maven__version=${MAVEN_VERSION}"
