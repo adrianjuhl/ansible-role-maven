023bffcb1cd0abf920fc4836cba0d22aaf1d1a36
Adrian Juhl <adrian.juhl@gmail.com>
2025-10-10 12:04:30 +1030

    Expose further role parameters in the install script.

--- .extras/bin/install_maven.sh
+++ .extras/bin/install_maven.sh
@@ -202,7 +202,7 @@ parse_script_params()
       msg "Error: Missing archive_file_checksum value: --archive_file_name"
       abort_script
     else
-      MAVEN_ARCHIVE_FILE_CHECKSUM_ANSIBLE_EXTRA_VARS_PARAM="--extra-vars=""adrianjuhl__maven__archive_file_checksum=${MAVEN_ARCHIVE_FILE_CHECKSUM_PARAM}"""
+      MAVEN_ARCHIVE_FILE_CHECKSUM_ANSIBLE_EXTRA_VARS_PARAM="--extra-vars=\"adrianjuhl__maven__archive_file_checksum=${MAVEN_ARCHIVE_FILE_CHECKSUM_PARAM}\""
     fi
   else
     MAVEN_ARCHIVE_FILE_CHECKSUM_ANSIBLE_EXTRA_VARS_PARAM="dummy"
