fe056ba443c333197433a55fa5eca1996ad626ed
Adrian Juhl <adrian.juhl@gmail.com>
2025-10-10 13:10:35 +1030

    Expose further role parameters in the install script.

--- .extras/bin/install_maven.sh
+++ .extras/bin/install_maven.sh
@@ -89,10 +89,10 @@ install_maven()
     ASK_BECOME_PASS_OPTION="--ask-become-pass"
   fi
 
-  echo "MAVEN_ARCHIVE_FILE_CHECKSUM_ANSIBLE_EXTRA_VARS_PARAM[@] is: >>${MAVEN_ARCHIVE_FILE_CHECKSUM_ANSIBLE_EXTRA_VARS_PARAM[@]}<<"
+  echo "MAVEN_ARCHIVE_FILE_CHECKSUM_ANSIBLE_EXTRA_VARS_PARAM is: >>${MAVEN_ARCHIVE_FILE_CHECKSUM_ANSIBLE_EXTRA_VARS_PARAM}<<"
 #    --extra-vars="adrianjuhl__maven__archive_file_checksum=sha512:706f01b20dec0305a822ab614d51f32b07ee11d0218175e55450242e49d2156386483b506b3a4e8a03ac8611bae96395fd5eec15f50d3013d5deed6d1ee18225" \
 
-  ANSIBLE_PLAYBOOK_COMMAND_OPTIONS=(
+  ANSIBLE_PLAYBOOK_COMMAND_OPTIONS_ARRAY_PRELIMINARY=(
     "${ANSIBLE_CHECK_MODE_ARGUMENT}"
     "${ANSIBLE_DIFF_MODE_ARGUMENT}"
     "${ANSIBLE_VERBOSE_ARGUMENT}" 
@@ -101,19 +101,19 @@ install_maven()
     "--connection=local"
     "--extra-vars=adrianjuhl__maven__version=${MAVEN_VERSION}"
     "--extra-vars=adrianjuhl__maven__archive_file_name=${MAVEN_ARCHIVE_FILE_NAME}"
-    "${MAVEN_ARCHIVE_FILE_CHECKSUM_ANSIBLE_EXTRA_VARS_PARAM[@]}"
+    "${MAVEN_ARCHIVE_FILE_CHECKSUM_ANSIBLE_EXTRA_VARS_PARAM}"
     "--extra-vars=adrianjuhl__maven__install_directory=${INSTALL_DIRECTORY}"
     "--extra-vars=local_playbook__install_maven__requires_become=${REQUIRES_BECOME}"
   )
-  echo "ANSIBLE_PLAYBOOK_COMMAND_OPTIONS[@] is: >>${ANSIBLE_PLAYBOOK_COMMAND_OPTIONS[@]}<<"
-  ANSIBLE_PLAYBOOK_COMMAND_OPTIONS_NON_EMPTY=()
-  for element in "${ANSIBLE_PLAYBOOK_COMMAND_OPTIONS[@]}"; do
+  echo "ANSIBLE_PLAYBOOK_COMMAND_OPTIONS_ARRAY_PRELIMINARY[@] is: >>${ANSIBLE_PLAYBOOK_COMMAND_OPTIONS_ARRAY_PRELIMINARY[@]}<<"
+  ANSIBLE_PLAYBOOK_COMMAND_OPTIONS_ARRAY=()
+  for element in "${ANSIBLE_PLAYBOOK_COMMAND_OPTIONS_ARRAY_PRELIMINARY[@]}"; do
     trimmed_element="${element// /}"
     if [ -n "${trimmed_element}" ]; then
-      ANSIBLE_PLAYBOOK_COMMAND_OPTIONS_NON_EMPTY+=("${trimmed_element}")
+      ANSIBLE_PLAYBOOK_COMMAND_OPTIONS_ARRAY+=("${trimmed_element}")
     fi
   done
-  echo "ANSIBLE_PLAYBOOK_COMMAND_OPTIONS_NON_EMPTY[@] is: >>${ANSIBLE_PLAYBOOK_COMMAND_OPTIONS_NON_EMPTY[@]}<<"
+  echo "ANSIBLE_PLAYBOOK_COMMAND_OPTIONS_ARRAY[@] is: >>${ANSIBLE_PLAYBOOK_COMMAND_OPTIONS_ARRAY[@]}<<"
 #  ansible-playbook ${ANSIBLE_CHECK_MODE_ARGUMENT} ${ANSIBLE_DIFF_MODE_ARGUMENT} ${ANSIBLE_VERBOSE_ARGUMENT} ${ASK_BECOME_PASS_OPTION} \
 #    --inventory="localhost," \
 #    --connection=local \
@@ -124,7 +124,7 @@ install_maven()
 #    --extra-vars=local_playbook__install_maven__requires_become="${REQUIRES_BECOME}" \
 #    ${EXTRAS_DIRECTORY}/.ansible/playbooks/install_maven.yml
   ansible-playbook \
-    "${ANSIBLE_PLAYBOOK_COMMAND_OPTIONS_NON_EMPTY[@]}" \
+    "${ANSIBLE_PLAYBOOK_COMMAND_OPTIONS_ARRAY[@]}" \
     ${EXTRAS_DIRECTORY}/.ansible/playbooks/install_maven.yml
 
 }
