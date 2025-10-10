b020fae0163f0f91ee1cf0451ea9aef25f5e3155
Adrian Juhl <adrian.juhl@gmail.com>
2025-10-10 11:56:20 +1030

    Expose further role parameters in the install script.

--- .extras/bin/install_maven.sh
+++ .extras/bin/install_maven.sh
@@ -96,7 +96,7 @@ install_maven()
     --connection=local \
     --extra-vars="adrianjuhl__maven__version=${MAVEN_VERSION}" \
     --extra-vars="adrianjuhl__maven__archive_file_name=${MAVEN_ARCHIVE_FILE_NAME}" \
-    --extra-vars="adrianjuhl__maven__archive_file_checksum=sha512:blah_from_command_line" \
+    --extra-vars="adrianjuhl__maven__archive_file_checksum=sha512:706f01b20dec0305a822ab614d51f32b07ee11d0218175e55450242e49d2156386483b506b3a4e8a03ac8611bae96395fd5eec15f50d3013d5deed6d1ee18225" \
     --extra-vars="adrianjuhl__maven__install_directory=${INSTALL_DIRECTORY}" \
     --extra-vars="local_playbook__install_maven__requires_become=${REQUIRES_BECOME}" \
     ${EXTRAS_DIRECTORY}/.ansible/playbooks/install_maven.yml
